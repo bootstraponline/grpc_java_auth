3f56194bcc12c82c227edbfeb388253d834d6b3f
bootstraponline <matt@bootstraponline.com>
2018-06-07 14:07:09 -0400

    Work on client/server TLS

--- soseedygrpc/src/main/kotlin/com/instructure/dataseeding/regular_tls/ClientTls.kt
+++ soseedygrpc/src/main/kotlin/com/instructure/dataseeding/regular_tls/ClientTls.kt
@@ -16,111 +16,46 @@
 
 package com.instructure.dataseeding.regular_tls
 
-import com.google.protobuf.ByteString
+import com.instructure.dataseeding.BaseClient
 import com.instructure.dataseeding.mutual_auth.Certs
-import com.instructure.soseedy.EchoGrpc
-import com.instructure.soseedy.EchoRequest
-import com.instructure.soseedy.EchoResponse
-import io.grpc.ManagedChannel
-import io.grpc.StatusRuntimeException
 import io.grpc.netty.GrpcSslContexts
 import io.grpc.netty.NegotiationType
 import io.grpc.netty.NettyChannelBuilder
 import io.netty.handler.ssl.SslContext
 import java.io.File
-import java.util.concurrent.TimeUnit
-import java.util.logging.Level
-import java.util.logging.Logger
 import javax.net.ssl.SSLException
 
 /**
  * A simple client that requests a greeting from the HelloWorldServer with TLS.
  */
-class ClientTls
-/**
- * Construct client for accessing RouteGuide server using the existing channel.
- */
-internal constructor(private val channel: ManagedChannel) {
-    private val blockingStub: EchoGrpc.EchoBlockingStub = EchoGrpc.newBlockingStub(channel)
+object ClientTls {
 
-    /**
-     * Construct client connecting to HelloWorld server at `host:port`.
-     */
+
     @Throws(SSLException::class)
-    constructor(host: String,
-                port: Int,
-                sslContext: SslContext) : this(NettyChannelBuilder.forAddress(host, port)
-            .negotiationType(NegotiationType.TLS)
-            .sslContext(sslContext)
-            .build()) {
+    private fun buildSslContext(trustCertCollectionFilePath: String?,
+                                clientCertChainFilePath: String?,
+                                clientPrivateKeyFilePath: String?): SslContext {
+        val builder = GrpcSslContexts.forClient()
+        if (trustCertCollectionFilePath != null) {
+            builder.trustManager(File(trustCertCollectionFilePath))
+        }
+        if (clientCertChainFilePath != null && clientPrivateKeyFilePath != null) {
+            builder.keyManager(File(clientCertChainFilePath), File(clientPrivateKeyFilePath))
+        }
+        return builder.build()
     }
 
-    @Throws(InterruptedException::class)
-    fun shutdown() {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
-    }
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val trustCertCollectionFilePath = Certs.trustCertCollectionFile.toString()
+        val sslContext = buildSslContext(trustCertCollectionFilePath, null, null)
 
-    /**
-     * Say hello to server.
-     */
-    fun greet(name: String) {
-        logger.info("Will try to greet $name ...")
-        val response: EchoResponse
-        try {
-            val request = EchoRequest.newBuilder().setText(name).build()
-            response = blockingStub.get(request)
-        } catch (e: StatusRuntimeException) {
-            logger.log(Level.WARNING, "RPC failed: {0}", e.status)
-            return
-        }
+        val channel = NettyChannelBuilder.forAddress("localhost", 50051)
+                .negotiationType(NegotiationType.TLS)
+                .sslContext(sslContext)
+                .build()
 
-        logger.info("Greeting: " + response.text)
-    }
-
-    companion object {
-        private val logger = Logger.getLogger(ClientTls::class.java.name)
-
-        @Throws(SSLException::class)
-        private fun buildSslContext(trustCertCollectionFilePath: String?,
-                                    clientCertChainFilePath: String?,
-                                    clientPrivateKeyFilePath: String?): SslContext {
-            val builder = GrpcSslContexts.forClient()
-            if (trustCertCollectionFilePath != null) {
-                builder.trustManager(File(trustCertCollectionFilePath))
-            }
-            if (clientCertChainFilePath != null && clientPrivateKeyFilePath != null) {
-                builder.keyManager(File(clientCertChainFilePath), File(clientPrivateKeyFilePath))
-            }
-            return builder.build()
-        }
-
-        /**
-         * Greet server. If provided, the first element of `args` is the name to use in the
-         * greeting.
-         */
-        @Throws(Exception::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            println("USAGE: ClientTls host port [trustCertCollectionFilePath] " +
-                    "[clientCertChainFilePath] [clientPrivateKeyFilePath]\n  Note: clientCertChainFilePath and " +
-                    "clientPrivateKeyFilePath are only needed if mutual auth is desired. And if you specify " +
-                    "clientCertChainFilePath you must also specify clientPrivateKeyFilePath")
-
-            val host = "localhost"
-            val port = 50051
-            val trustCertCollectionFilePath = Certs.trustCertCollectionFile.toString()
-
-            val client = ClientTls(host, port,
-                    buildSslContext(trustCertCollectionFilePath, null, null))
-
-
-            try {
-                /* Access a service running on the local machine on port 50051 */
-                var user = "world"
-                client.greet(user)
-            } finally {
-                client.shutdown()
-            }
-        }
+        BaseClient(channel).greetAndShutdown()
     }
 }
--- soseedygrpc/src/main/kotlin/com/instructure/dataseeding/regular_tls/ServerTls.kt
+++ soseedygrpc/src/main/kotlin/com/instructure/dataseeding/regular_tls/ServerTls.kt
@@ -1,107 +1,31 @@
 package com.instructure.dataseeding.mutual_auth
 
-/*
- * Copyright 2015 The gRPC Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+import com.instructure.dataseeding.BaseServer
 import com.instructure.dataseeding.EchoGrpcImpl
-import io.grpc.Server
 import io.grpc.netty.GrpcSslContexts
 import io.grpc.netty.NettyServerBuilder
-import io.netty.handler.ssl.ClientAuth
 import io.netty.handler.ssl.SslContextBuilder
 import io.netty.handler.ssl.SslProvider
-import java.io.File
-import java.io.IOException
 import java.net.InetSocketAddress
-import java.util.logging.Logger
 
-/** Server that manages startup/shutdown of a `Greeter` server with TLS enabled.  */
-class ServerTls(
-        private val host: String,
-        private val port: Int,
-        private val certChainFilePath: String,
-        private val privateKeyFilePath: String) {
-
-    private var server: Server? = null
+object ServerTls {
 
     private val sslContextBuilder: SslContextBuilder
         get() {
-            val sslClientContextBuilder = SslContextBuilder.forServer(File(certChainFilePath), File(privateKeyFilePath))
+            val sslClientContextBuilder = SslContextBuilder.forServer(Certs.certChainFile, Certs.privateKeyFile)
             return GrpcSslContexts.configure(sslClientContextBuilder, SslProvider.OPENSSL)
         }
 
-    @Throws(IOException::class)
-    private fun start() {
-        server = NettyServerBuilder.forAddress(InetSocketAddress(host, port))
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val nettyServer = NettyServerBuilder.forAddress(InetSocketAddress("localhost", 50051))
                 .addService(EchoGrpcImpl())
                 .sslContext(sslContextBuilder.build())
+                .useTransportSecurity(Certs.certChainFile, Certs.privateKeyFile)
                 .build()
-                .start()
-        logger.info("Server started, listening on $port")
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        object : Thread() {
-                            override fun run() {
-                                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                                System.err.println("*** shutting down gRPC server since JVM is shutting down")
-                                this@ServerTls.stop()
-                                System.err.println("*** server shut down")
-                            }
-                        })
-    }
 
-    private fun stop() {
-        if (server != null) {
-            server!!.shutdown()
-        }
-    }
-
-    /** Await termination on the main thread since the grpc library uses daemon threads.  */
-    @Throws(InterruptedException::class)
-    private fun blockUntilShutdown() {
-        if (server != null) {
-            server!!.awaitTermination()
-        }
-    }
-
-    companion object {
-        private val logger = Logger.getLogger(MutualAuthServerTls::class.java.name)
-
-        /** Main launches the server from the command line.  */
-        @Throws(IOException::class, InterruptedException::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-
-            println(
-                    "USAGE: ServerTls host port certChainFilePath privateKeyFilePath "
-                            + "[clientCertChainFilePath]\n  Note: You only need to supply clientCertChainFilePath if you want "
-                            + "to enable Mutual TLS.")
-
-            val host = "localhost"
-            val port = 50051
-            val certChainFilePath = Certs.certChainFile.toString()
-            val privateKeyFilePath = Certs.privateKeyFile.toString()
-
-            println("Starting ServerTls")
-
-            val server = ServerTls(
-                    host, port, certChainFilePath, privateKeyFilePath)
-            server.start()
-            server.blockUntilShutdown()
-        }
+        val server = BaseServer(nettyServer)
+        server.startAndBlockUntilShutdown()
     }
 }
-
