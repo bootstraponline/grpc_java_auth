c8cc60ccf0b071affac8cde2083a5e1ae2c622a3
Nate Armstrong <narmstrong@instructure.com>
2018-06-08 08:44:45 -0600

    Tests passing

--- soseedygrpc/openssl/keys.sh
+++ soseedygrpc/openssl/keys.sh
@@ -4,15 +4,15 @@
 
 # Changes these CN's to match your hosts in your environment if needed.
 SERVER_CN=example.com
-CLIENT_CN=example.com # Used when doing mutual TLS
+CLIENT_CN=localhost # Used when doing mutual TLS
 
 echo Generate CA key:
-openssl genrsa -out ca.key 4096
+openssl genrsa -out ca.key 2048
 echo Generate CA certificate:
 # Generates ca.crt which is the trustCertCollectionFile
-openssl req  -new -x509 -days 365 -key ca.key -out ca.crt -subj "/CN=${SERVER_CN}"
+openssl req  -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=US/ST=Foo/L=Bar/O=Baz/CN=Xyz"
 echo Generate server key:
-openssl genrsa -out server.key 4096
+openssl genrsa -out server.key 2048
 echo Generate server signing request:
 openssl req  -new -key server.key -out server.csr -subj "/CN=${SERVER_CN}"
 echo Self-signed server certificate:
@@ -20,13 +20,14 @@ echo Self-signed server certificate:
 openssl x509 -req  -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 echo Remove passphrase from server key:
 openssl rsa  -in server.key -out server.key
+
 echo Generate client key
-openssl genrsa -out client.key 4096
+openssl genrsa -out client.key 2048
 echo Generate client signing request:
 openssl req  -new -key client.key -out client.csr -subj "/CN=${CLIENT_CN}"
 echo Self-signed client certificate:
 # Generates client.crt which is the clientCertChainFile for the client (need for mutual TLS only)
-openssl x509  -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+openssl x509  -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
 echo Remove passphrase from client key:
 openssl rsa  -in client.key -out client.key
 echo Converting the private keys to X.509:
