78ddbd75d45340389cc6a34884a474a4a389383e
bootstraponline <matt@bootstraponline.com>
2018-06-09 00:08:16 -0400

    Use certstrap

--- soseedygrpc/openssl/keys.sh
+++ soseedygrpc/openssl/keys.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
+rm -rf *.crl *.key *.crt *.csr
+
+go get -u github.com/square/certstrap
+
+# See mutual auth generation script:
 # https://github.com/grpc/grpc-java/tree/master/examples
 
-# Changes these CN's to match your hosts in your environment if needed.
-SERVER_CN=example.com
-CLIENT_CN=localhost # Used when doing mutual TLS
+SIZE=2048
 
-echo Generate CA key:
-openssl genrsa -out ca.key 2048
-echo Generate CA certificate:
-# Generates ca.crt which is the trustCertCollectionFile
-openssl req  -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=US/ST=Foo/L=Bar/O=Baz/CN=Xyz"
-echo Generate server key:
-openssl genrsa -out server.key 2048
-echo Generate server signing request:
-openssl req  -new -key server.key -out server.csr -subj "/CN=${SERVER_CN}"
-echo Self-signed server certificate:
-# Generates server.crt which is the certChainFile for the server
-openssl x509 -req  -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
-echo Remove passphrase from server key:
-openssl rsa  -in server.key -out server.key
+# CA
+certstrap --depot-path . init --passphrase '' --common-name example.com --key-bits $SIZE
+mv ./example.com.key ./ca.key
+mv ./example.com.crt ./ca.crt
+mv ./example.com.crl ./ca.crl
 
-echo Generate client key
-openssl genrsa -out client.key 2048
-echo Generate client signing request:
-openssl req  -new -key client.key -out client.csr -subj "/CN=${CLIENT_CN}"
-echo Self-signed client certificate:
-# Generates client.crt which is the clientCertChainFile for the client (need for mutual TLS only)
-openssl x509  -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
-echo Remove passphrase from client key:
-openssl rsa  -in client.key -out client.key
-echo Converting the private keys to X.509:
-# Generates client.pem which is the clientPrivateKeyFile for the Client (needed for mutual TLS only)
-openssl pkcs8 -topk8 -nocrypt -in client.key -out client.pem
-# Generates server.pem which is the privateKeyFile for the Server
+# Server
+certstrap --depot-path . request-cert --passphrase '' --common-name example.com --domain 'example.com'  --key-bits $SIZE
+mv ./example.com.key server.key
+mv ./example.com.csr server.csr
+
+certstrap --depot-path . sign server --CA ca
 openssl pkcs8 -topk8 -nocrypt -in server.key -out server.pem
+
+# Client
+certstrap --depot-path . request-cert --passphrase '' --common-name example.com --domain 'example.com'  --key-bits $SIZE
+mv ./example.com.key client.key
+mv ./example.com.csr client.csr
+
+certstrap --depot-path . sign client --CA ca
+openssl pkcs8 -topk8 -nocrypt -in client.key -out client.pem
+
+# netty only supports PKCS8 keys. openssl is used to convert from PKCS1 to PKCS8
+# http://netty.io/wiki/sslcontextbuilder-and-private-key.html
